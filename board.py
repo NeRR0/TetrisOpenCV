2bcd6299e1c0f25bce33a84e4ebe06dabe2d8b7a
Dawid <dawid.brozek@skillforce.pl>
2021-08-27 13:19:55 +0200

    init board

---
+++ board.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+class Board:
+    def __init__(self, width, heigth):
+        self._width = width
+        self._heigth = heigth
+        self._background = None
+        self._game_counter = 0
+
+    def create(self):
+        self._background = np.zeros((self._width, self._heigth, 3), dtype=np.uint8)
+        return self._background
+
+    def counter(self):
+        self._game_counter +=1
+        if self._game_counter == 250:
+            return True
+        else:
+            return False
